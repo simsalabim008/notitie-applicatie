c46e04c07849ec90ff7db4a3542a07c97f3ee682
Simon <westra77@gmail.com>
2017-09-24 14:10:20 +0200

    Add files via upload

---
+++ notitiedata/DBConst.java
@@ -0,0 +1,11 @@
+package notitiedata;
+
+public class DBConst {
+     
+    public static final String DRIVERNAAM = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql:website:port/database-name";
+    public static final String GEBRUIKERSNAAM = "username";
+    public static final String WACHTWOORD = "password";
+
+
+}
